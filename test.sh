fa83e316a1094cca9bef9161833bc9acac90d293
Tilen Tomakić <tilen@tomakic.com>
2017-12-12 09:24:23 +0100

    Improve docker-compose tests so they don't depend on random service id.

--- test.sh
+++ test.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
 docker-compose -f docker-compose.test.yml build
-docker-compose -f docker-compose.test.yml up --abort-on-container-exit
-TEST_STATUS=`docker wait apiseed_app-test_1`
+docker-compose -f docker-compose.test.yml up --exit-code-from app-test
 
-if [ ${TEST_STATUS} -eq 0 ]; then
+if [ $? -eq 0 ]; then
     echo "TESTS PASSED"
 else
     echo "TESTS FAILED"
