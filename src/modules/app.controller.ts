e808b551c3b64029a02d23cb0e4446ffdb0a4713
Tilen Tomakić <tilen@tomakic.com>
2017-12-13 15:29:33 +0100

    Add kumoluze microservice.

--- src/modules/app.controller.ts
+++ src/modules/app.controller.ts
@@ -13,16 +13,36 @@ export interface ReportInfo {
 }
 
 const seminarReportInfo: ReportInfo = {
-  'clani'        : ['tt5157'],
+  'clani'        : [ 'tt5157' ],
   'opis_projekta': 'Projekt implementira aplikacijo sestavljeno iz več mikrostoritev za zbiranje podatkov iz več virov med drugimi tudi twitter (mini spletni pajek). Za beleženje se uporablja custom ELK stack dosegljiv na http://log.tomakic.com/elk/app/kiban',
   'mikrostoritve': [
-    'http://35.198.123.57:3000/job', 'http://35.198.182.182:3000', 'http://35.198.100.92:3000/twitter', 'http://35.198.149.151:3000/web'],
+    'http://35.198.123.57:3000/job',
+    'http://35.198.182.182:3000',
+    'http://35.198.100.92:3000/twitter',
+    'http://35.198.149.151:3000/web',
+    'http://35.198.160.123:8081/v1/orders'
+  ],
   'github'       : [
-    'https://github.com/NestJsPlayground/JobMicroService', 'https://github.com/TilenTomakic/nestjs-seed', 'https://github.com/NestJsPlayground/TwitterMicroService', 'https://github.com/NestJsPlayground/WebMicroService'],
+    'https://github.com/NestJsPlayground/JobMicroService',
+    'https://github.com/TilenTomakic/nestjs-seed',
+    'https://github.com/NestJsPlayground/TwitterMicroService',
+    'https://github.com/NestJsPlayground/WebMicroService',
+    'https://github.com/NestJsPlayground/JobOrderMicroService'
+  ],
   'travis'       : [
-    'https://travis-ci.org/NestJsPlayground/JobMicroService', 'https://travis-ci.org/TilenTomakic/nestjs-seed', 'https://travis-ci.org/NestJsPlayground/TwitterMicroService', 'https://travis-ci.org/NestJsPlayground/WebMicroService'],
+    'https://travis-ci.org/NestJsPlayground/JobMicroService',
+    'https://travis-ci.org/TilenTomakic/nestjs-seed',
+    'https://travis-ci.org/NestJsPlayground/TwitterMicroService',
+    'https://travis-ci.org/NestJsPlayground/WebMicroService',
+    'https://travis-ci.org/NestJsPlayground/JobOrderMicroService'
+  ],
   'dockerhub'    : [
-    'https://hub.docker.com/r/tilen/demo-svc-job/', 'https://hub.docker.com/r/tilen/nestjs-seed/', 'https://hub.docker.com/r/tilen/demo-svc-twitter/', 'https://hub.docker.com/r/tilen/demo-svc-web/']
+    'https://hub.docker.com/r/tilen/demo-svc-job/',
+    'https://hub.docker.com/r/tilen/nestjs-seed/',
+    'https://hub.docker.com/r/tilen/demo-svc-twitter/',
+    'https://hub.docker.com/r/tilen/demo-svc-web/',
+    'https://hub.docker.com/r/tilen/job-orders/'
+  ]
 };
 
 @Controller()
